d6b52ff1b9d5d75a467524d4366b090fe9044f46
Jay Sanduke <jaysanduke@gmail.com>
2021-08-29 21:48:57 +0530

    commit

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -16,3 +16,19 @@ on('click', '.navbar .dropdown > a', function (e) {
     this.nextElementSibling.classList.toggle('dropdown-active')
   }
 }, true)
+
+/**
+   * Back to top button
+   */
+ let backtotop = select('.back-to-top')
+ if (backtotop) {
+   const toggleBacktotop = () => {
+     if (window.scrollY > 100) {
+       backtotop.classList.add('active')
+     } else {
+       backtotop.classList.remove('active')
+     }
+   }
+   window.addEventListener('load', toggleBacktotop)
+   onscroll(document, toggleBacktotop)
+ }
